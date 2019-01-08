42c68061b571d87364e6c358da8ea2d218a7604e
SimonChapple <40208173+SimonChapple@users.noreply.github.com>
2019-01-08 20:41:21 +0000

    Added include for MicrobitIBeacon.h header file.

--- source/bluetooth/MicroBitBLEManager.cpp
+++ source/bluetooth/MicroBitBLEManager.cpp
@@ -29,6 +29,7 @@ DEALINGS IN THE SOFTWARE.
 #include "MicroBitStorage.h"
 #include "MicroBitFiber.h"
 #include "MicroBitSystemTimer.h"
+#include "MicroBitIBeacon.h"
 
 /* The underlying Nordic libraries that support BLE do not compile cleanly with the stringent GCC settings we employ.
  * If we're compiling under GCC, then we suppress any warnings generated from this code (but not the rest of the DAL)
